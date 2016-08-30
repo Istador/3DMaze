5691ccacca6f81bba7e0ed4cff3bf0a234d63a75
Robin C. Ladiges <rcl@blackpinguin.de>
2016-08-30 17:15:16 +0200

    first commit

---
+++ WallWalking/Assets/Movement.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	WallCollide wc;
+	Camera cam;
+	Rigidbody rb;
+	Vector3 Pos { get { return transform.position; } set { transform.position = value; } }
+	Quaternion Rot { get { return transform.rotation; } set { transform.rotation = value; } }
+
+	private float moveSpeed = 6; // move speed
+	private float jumpSpeed = 12; // vertical jump initial speed
+
+	private double jumpCooldown = 0.0;
+
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		wc = GetComponent<WallCollide> ();
+		cam = GetComponentInChildren<Camera> ();
+	}
+
+	void Update () {
+		if (wc.Grounded) {
+			//TODO: slow down if unpressed
+			rb.drag = 2;
+
+			//rb.velocity = Vector3.zero;
+			//rb.velocity += transform.forward * Input.GetAxis ("Vertical") * moveSpeed * rb.mass * 50 * Time.deltaTime;
+			//rb.velocity += transform.right * Input.GetAxis ("Horizontal") * moveSpeed * rb.mass * 50 * Time.deltaTime;
+
+
+			rb.AddForce (transform.forward * Input.GetAxis ("Vertical") * moveSpeed * rb.mass * 80 * Time.deltaTime);
+			rb.AddForce (transform.right * Input.GetAxis ("Horizontal") * moveSpeed * rb.mass * 80 * Time.deltaTime);
+
+			// todo jump cooldown
+			if (Input.GetButtonDown ("Jump") && jumpCooldown <= 0.0) {
+				rb.AddForce (jumpSpeed * rb.mass * cam.transform.forward, ForceMode.Impulse);
+				//rb.velocity += jumpSpeed * rb.mass * cam.transform.forward;
+				jumpCooldown = 1.0;
+			} else {
+				jumpCooldown = System.Math.Max(0.0, jumpCooldown - Time.deltaTime);
+			}
+
+			// gravity if near walls
+			Ray ray;
+			RaycastHit hit;
+			ray = new Ray (Pos, -wc.Up);
+			if (Physics.Raycast (ray, out hit, WallCollide.Radius, WallCollide.Walls)) {
+				//rb.velocity += WallCollide.Gravity * rb.mass * -wc.Up * hit.distance / WallCollide.Radius * 50 * Time.deltaTime;
+				rb.AddForce(WallCollide.Gravity * rb.mass * -wc.Up * hit.distance / WallCollide.Radius * 50 * Time.deltaTime);
+			}
+
+			//transform.Translate (Vector3.forward * Input.GetAxis ("Vertical") * moveSpeed * Time.deltaTime);
+			//transform.Translate (Vector3.right * Input.GetAxis ("Horizontal") * moveSpeed * Time.deltaTime);
+		} else {
+			rb.drag = 0;
+		}
+	}
+}
---
+++ WallWalking/Assets/WallCollide.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallCollide : MonoBehaviour {
+	Rigidbody rb;
+
+
+	public const int Walls = 1 << 8;
+	public const float Gravity = 3f;
+	public const float Radius = 2.5f;
+
+	public bool Grounded { get; private set; }
+	public Vector3 Up { get; private set; }
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+		Up = transform.up;
+	}
+
+	void FixedUpdate() {
+		Collider[] hits = Physics.OverlapSphere(transform.position, Radius, Walls);
+		string o = "";
+		Vector3 Pos = rb.worldCenterOfMass;
+		Vector3 up = Vector3.zero;
+		Grounded = hits.Length > 0;
+
+		foreach (Collider hit in hits) {
+			Vector3 HitPos = hit.ClosestPointOnBounds (Pos);
+			float dist = Vector3.Distance (Pos, HitPos);
+			o += hit.name + ": " + dist + ", ";
+
+			Vector3 v = Pos - HitPos;
+			v = v.normalized * (1f / v.magnitude);
+			Debug.DrawLine (HitPos, Pos, Color.green);
+
+			up += v;
+		}
+		Debug.Log (o);
+
+		up.Normalize();
+		if (up != Vector3.zero) {
+			Up = Vector3.Slerp (Up, up, 3f * Time.fixedDeltaTime);
+		}
+
+		Debug.DrawLine (Pos, Pos + Up * 3f, Color.red);
+		Debug.DrawLine (Pos, Pos + transform.forward * 3f, Color.blue);
+
+		/*if (Grounded) {
+			// gravity
+			Ray ray;
+			RaycastHit hit;
+			ray = new Ray (Pos, -Up);
+			if (Physics.Raycast (ray, out hit, Radius, Walls)) {
+				rb.AddForce(Gravity * rb.mass * -Up * hit.distance / Radius);
+			}
+		}*/
+
+		float lerpSpeed = 15.5f;
+		Vector3 myNormal = Vector3.Lerp(transform.up, up, lerpSpeed * Time.fixedDeltaTime);
+		Vector3 myForward = Vector3.Cross(transform.right, myNormal);
+		Quaternion targetRot = Quaternion.LookRotation(myForward, myNormal);
+		//transform.rotation = targetRot;
+		transform.rotation = Quaternion.Lerp(transform.rotation, targetRot, lerpSpeed * Time.fixedDeltaTime);
+
+		/*
+		//Quaternion q = Quaternion.FromToRotation (transform.up, Up);
+		//Quaternion q = Quaternion.FromToRotation (transform.up, Up);
+		//transform.rotation = Quaternion.Slerp(transform.rotation, q, 0.5f * Time.fixedDeltaTime);
+
+		//rb.AddForce (transform.forward * Input.GetAxis ("Horizontal") * Time.fixedDeltaTime * 8f, ForceMode.Acceleration);
+		//rb.AddForce (transform.right * Input.GetAxis ("Vertical") * Time.fixedDeltaTime * -8f, ForceMode.Acceleration);
+		*/
+	}
+
+	/*void OnCollisionEnter (Collision col) {
+		Debug.Log (col.gameObject.name);
+	}*/
+}
